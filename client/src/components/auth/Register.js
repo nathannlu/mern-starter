7c6265544f56809dbb821489687a6f2dc19691f9
Nathan <nathanlu_@hotmail.com>
2020-04-27 13:39:01 -0700

    Update Register.js

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -5,7 +5,7 @@ import { connect } from 'react-redux';
 import { registerUser } from '../../actions/authActions';
 import classnames from 'classnames';
 
-const Register = () => {
+const Register = props => {
   const initialFormState = {
     name: '',
     email: '',
